54f30b62d004f0da296326ffec3fca0250868e28
Andrea Grillo <andrea.grillo96@live.com>
2016-10-11 17:37:12 +0200

    untested es01 algorithm

--- Block2/Exercise01/Program.cs
+++ Block2/Exercise01/Program.cs
@@ -7,5 +7,20 @@ namespace Block2
         static void Main(string[] args)
         {
         }
+
+        static int getMin(int a, int b)
+        {
+            int[] array = { a, b };
+            int diff = a - b;
+            int sign = diff / Math.Abs(diff);
+            int index = (int)(0.5 + sign * 0.5);
+            return array[index];
+        }
+
+        static int getMax(int a, int b)
+        {
+            int[] array = { a, b };
+            return array[(int)(0.5 - ((a - b) / Math.Abs(a - b)) * 0.5)];
+        }
     }
 }
