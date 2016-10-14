f171a73bc0737b5e4b1217d1b4b472d3792926a7
andregri <andrea.grillo96@live.com>
2016-10-14 16:28:20 +0200

    fix equation solver

--- Block2/Exercise03/Program.cs
+++ Block2/Exercise03/Program.cs
@@ -34,17 +34,17 @@ namespace Exercise3
             double firstRoot, secondRoot;
 
             double discriminant = getDiscriminant(a, b, c);
-            if (discriminant > 0)
+            if (discriminant >= 0)
             {
                 firstRoot = (-b + Math.Sqrt(discriminant)) / (2 * a);
                 secondRoot = (-b - Math.Sqrt(discriminant)) / (2 * a);
 
-                Console.WriteLine("Real solution are: {0:f4} or {1:f4}", firstRoot, secondRoot);
+                Console.WriteLine("Real solution are: {0:f4} or {1:f4}\n", firstRoot, secondRoot);
             }
 
             else
             {
-                Console.WriteLine("Equation hasn't real solution");
+                Console.WriteLine("Equation hasn't real solution\n");
             }
         }
 
