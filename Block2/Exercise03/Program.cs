ddb729d55bf50e08b125eaea3bae6e6d2ca86b4e
michisalvo <jimmyginooo@libero.it>
2016-10-14 15:54:41 +0200

    Testing discriminant

--- Block2/Exercise03/Program.cs
+++ Block2/Exercise03/Program.cs
@@ -2,10 +2,33 @@
 
 namespace Exercise3
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            
+        }
+
+        public static void solveQuadraticEq(double a, double b, double c)
+        {
+            double firstRoot, secondRoot;
+
+            double discriminant = getDiscriminant(a, b, c);
+            if (discriminant > 0)
+            {
+                // firstRoot, secondRoot;
+            }
+
+            else
+            {
+
+            }
+        }
+
+        // creating for debugging purpose 
+        public static double getDiscriminant(double a, double b, double c)
+        {
+            return ((b * b) - (4 * a * c));
         }
     }
 }
--- Block2/UnitTest/UnitTest03.cs
+++ Block2/UnitTest/UnitTest03.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using Exercise3;
 namespace UnitTest
 {
     [TestClass]
     public class UnitTest03
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestPositiveDiscriminant()
         {
+            double test = Program.getDiscriminant(4, 9, 2);
+
+            Assert.AreEqual(49, test, "Error: result shouldn't be " + test);
         }
     }
 }
