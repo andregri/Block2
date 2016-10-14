9a7caa0a03369a4c295e3dfd58185a03953a7c0b
michisalvo <jimmyginooo@libero.it>
2016-10-14 11:49:01 +0200

    Main function untested

--- Block2/Exercise02/Program.cs
+++ Block2/Exercise02/Program.cs
@@ -6,6 +6,24 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
+            int counter = 0;
+            int counterMax = 100;
+            ulong firstNumber = 0;
+            ulong secondNumber = 1;
+
+            Fibonacci(counter, counterMax, firstNumber, secondNumber);
+        }
+
+        public static void Fibonacci(int counter, int counterMax, ulong previousNumber, ulong number)
+        {
+            ulong sum = number + previousNumber;
+            Console.Write("{0,3}: {1}\n", counter,sum);
+            if (counter < counterMax)
+            {
+                Fibonacci(++counter,counterMax, number, sum);
+            }
         }
     }
 }
+
+
