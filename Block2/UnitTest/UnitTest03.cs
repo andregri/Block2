2ca29289a701bdae01152f3d558a5e0d263a8103
andregri <andrea.grillo96@live.com>
2016-10-14 16:02:15 +0200

    complete discriminant testing

--- Block2/UnitTest/UnitTest03.cs
+++ Block2/UnitTest/UnitTest03.cs
@@ -9,9 +9,22 @@ namespace UnitTest
         [TestMethod]
         public void TestPositiveDiscriminant()
         {
-            double test = Program.getDiscriminant(4, 9, 2);
+            double delta = Program.getDiscriminant(4, 9, 2);
+            Assert.AreEqual(49, delta, "Error: result shouldn't be " + delta);
+        }
 
-            Assert.AreEqual(49, test, "Error: result shouldn't be " + test);
+        [TestMethod]
+        public void TestZeroDiscriminant()
+        {
+            double delta = Program.getDiscriminant(1, 2, 1);
+            Assert.AreEqual(0, delta, "Error: discriminant should be " + 0);
+        }
+
+        [TestMethod]
+        public void TestNegativeDiscriminant()
+        {
+            double delta = Program.getDiscriminant(1, 1, 1);
+            Assert.AreEqual(-3, delta, "Error: discriminant should be " + -3);
         }
     }
 }
