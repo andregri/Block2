b7d0754e95bd757728e5c1e5589cddcb48b0fc01
andregri <andrea.grillo96@live.com>
2016-10-17 20:11:15 +0200

    add test for function b

--- Block2/UnitTest/UnitTest10.cs
+++ Block2/UnitTest/UnitTest10.cs
@@ -8,7 +8,7 @@ namespace UnitTest
     public class UnitTest10
     {
         [TestMethod]
-        public void initMatrixBTest()
+        public void initMatrixBTestSize2()
         {
             int[,] result = { { 1, 4 }, { 2, 3 } };
             int[,] actual = Program.initMatrixB(2);
@@ -16,6 +16,22 @@ namespace UnitTest
             Assert.AreEqual(2, actual[1, 0]);
             Assert.AreEqual(3, actual[1, 1]);
             Assert.AreEqual(4, actual[0, 1]);
+        }
+
+        [TestMethod]
+        public void initMatrixBTestSize3()
+        {
+            int[,] result = { { 1, 6, 7 }, { 2, 5, 8 }, { 3, 4, 9 } };
+            int[,] actual = Program.initMatrixB(3);
+            Assert.AreEqual(1, actual[0, 0]);
+            Assert.AreEqual(2, actual[1, 0]);
+            Assert.AreEqual(3, actual[2, 0]);
+            Assert.AreEqual(4, actual[2, 1]);
+            Assert.AreEqual(5, actual[1, 1]);
+            Assert.AreEqual(6, actual[0, 1]);
+            Assert.AreEqual(7, actual[0, 2]);
+            Assert.AreEqual(8, actual[1, 2]);
+            Assert.AreEqual(9, actual[2, 2]);
         }
     }
 }
