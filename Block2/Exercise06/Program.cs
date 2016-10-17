d384c69a8dceb7db1a6526a92cde6f2f76b9767f
michisalvo <jimmyginooo@libero.it>
2016-10-17 20:51:51 +0200

    Added exercise, 2/4 methods, no main, no test

--- Block2/Exercise06/Program.cs
+++ Block2/Exercise06/Program.cs
@@ -7,5 +7,20 @@ namespace Exercise6
         static void Main(string[] args)
         {
         }
+
+        public static string decimalToBinary(int number)
+        {
+            string bin = Convert.ToString(number, 2);
+            return bin;
+        }
+
+
+        public static int binaryToDecimal(string number)
+        {
+            int dec = Convert.ToInt32(number, 2);
+            return dec;
+        }
+
+        public static
     }
 }
