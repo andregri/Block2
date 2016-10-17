46a20b290b3da89d20033a601bcb011bfea1ad36
michisalvo <jimmyginooo@libero.it>
2016-10-17 20:56:20 +0200

    Fixed some issues

--- Block2/Exercise04/Program.cs
+++ Block2/Exercise04/Program.cs
@@ -28,21 +28,6 @@ namespace Exercise04
             if (number == 0)
                 return "zero";
 
-            /*			// if we want expand range from 1 to 1000000 
-                        if ((number / 1000000) > 0 )
-                        {
-                            word += numberToWords(number / 1000000) + " million ";
-                            number %= 1000000;
-                        }
-
-                        // if we want expand range from 1 to 1000
-                        if ((number / 1000) > 0)
-                        {
-                            word += numberToWords(number / 1000) + " thousand ";
-                            number %= 1000;
-
-                        }
-            */
             if ((number / 100) > 0)
             {
                 word += numberToWords(number / 100) + " hundred ";
@@ -54,18 +39,18 @@ namespace Exercise04
                 if (word != "")
                     word += "and ";
 
-                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
-                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+                string[] units = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                string[] tens =  { "zero", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety" };
 
 
                 if (number < 20)
-                    word += " " + unitsMap[number];
+                    word += " " + units[number];
 
                 else
                 {
-                    word += tensMap[number / 10];
+                    word += tens[number / 10];
                     if ((number % 10) > 0)
-                        word += "-" + unitsMap[number % 10];
+                        word += "-" + units[number % 10];
                 }
 
             }
