a18887f43674ab0ab464e8e0cd5eeacad83e19dd
andregri <andrea.grillo96@live.com>
2016-10-17 19:28:54 +0200

    add matrix b function

--- Block2/Exercise10/Program.cs
+++ Block2/Exercise10/Program.cs
@@ -2,10 +2,69 @@
 
 namespace Exercise10
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+        }
+
+        public static int[,] initMatrixB(int size)
+        {
+            int[,] matrix = new int[size, size];
+
+            int row = 0;
+            int col = 0;
+
+            //decide if increasing(1) or decreasing(-1) row
+            int flag = 1;
+
+            for (int element = 0; element < size * size; element++)
+            {
+                matrix[row, col] = element + 1; //assign values from 1
+
+                row += flag;
+
+                if (row == size || ((row == -1) && (col != 0)))
+                {
+                    //pass to the next column
+                    col++;
+
+                    //invert direction of assigning values to rows
+                    flag *= -1;
+
+                    //avoid array bound exception 
+                    row += flag;
+
+                    /*
+                    if (row == size)
+                    {
+                        row--;
+                    }
+                    else
+                    {
+                        row++;
+                    }*/
+                }
+            }
+
+            return matrix;
+        }
+
+        public static void printMatrix(int[,] matrix)
+        {
+            if (matrix == null)
+            {
+                return;
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- Block2/UnitTest/UnitTest10.cs
+++ Block2/UnitTest/UnitTest10.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise10;
 
 namespace UnitTest
 {
@@ -7,8 +8,14 @@ namespace UnitTest
     public class UnitTest10
     {
         [TestMethod]
-        public void TestMethod1()
+        public void initMatrixBTest()
         {
+            int[,] result = { { 1, 4 }, { 2, 3 } };
+            int[,] actual = Program.initMatrixB(2);
+            Assert.AreEqual(1, actual[0, 0]);
+            Assert.AreEqual(2, actual[1, 0]);
+            Assert.AreEqual(3, actual[1, 1]);
+            Assert.AreEqual(4, actual[0, 1]);
         }
     }
 }
