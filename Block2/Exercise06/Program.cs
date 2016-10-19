ed2006ac5a9e26d8504a00bd6a1e58471f257ea8
michisalvo <jimmyginooo@libero.it>
2016-10-19 19:30:56 +0200

    Complete exercise, no test

--- Block2/Exercise06/Program.cs
+++ Block2/Exercise06/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Exercise6
 {
@@ -6,6 +7,35 @@ namespace Exercise6
     {
         static void Main(string[] args)
         {
+            string valueStr;
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a number (hex,binary or decimal form):");
+                valueStr = Console.ReadLine();
+
+                if (isBinary(valueStr) == true)
+                {
+                    Console.WriteLine("- Conversion to decimal is {0,3}\n", binaryToDecimal(valueStr));
+                }
+
+                else if (isDecimal(valueStr) == true)
+                {
+                    int temp = int.Parse(valueStr);
+                    Console.WriteLine("- Conversion to hex is {0,3}", decimalToHex(temp));
+                    Console.WriteLine("- Conversion to bin is {0,3}\n", decimalToBinary(temp));
+                }
+
+                else if (isHexadecimal(valueStr) == true)
+                {
+                    Console.WriteLine("- Conversion to decimal is {0,3}\n", hexToDecimal(valueStr));
+                }
+
+                else
+                {
+                    Console.WriteLine("Error. You don't have insert a valid format.\nRetry");
+                }
+            }
         }
 
         public static string decimalToBinary(int number)
@@ -21,6 +51,79 @@ namespace Exercise6
             return dec;
         }
 
-        public static
+
+        public static string decimalToHex(int number)
+        {
+            string hex = number.ToString("X");
+            return hex;
+        }
+
+        public static int hexToDecimal(string number)
+        {
+            int dec = int.Parse(number, NumberStyles.HexNumber);
+            return dec;
+        }
+
+
+        // check if a given string contain decimal number rappresentation
+        public static bool isDecimal(string str)
+        {
+            try
+            {
+                str = str.Trim();
+                decimal check = decimal.Parse(str);
+                return (true);
+            }
+            catch (FormatException)
+            {
+                // not a numeric value
+                return (false);
+            }
+
+        }
+
+        // check if a given string contain only hex number rappresentation 
+        public static bool isHexadecimal(string str)
+        {
+            try
+            {
+                str = str.Trim();
+                int check = int.Parse(str, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+                return (true);
+            }
+
+            catch (FormatException)
+            {
+                // not a numeric value
+                return (false);
+            }
+        }
+
+
+        // check if a given string contain only bin number rappresentation
+        public static bool isBinary(string str)
+        {
+            try
+            {
+                str = str.Trim();
+                int controlGuard = 0;
+
+                for (int i = 0; i < str.Length; i++)
+                {
+                    // parse each char of the passed string
+                    controlGuard = int.Parse(str[i].ToString());
+
+                    if ((controlGuard != 1) || (controlGuard != 0))
+                        return (false);
+                }
+                return (true);
+            }
+
+            catch (FormatException)
+            {
+                // not a numeric value
+                return (false);
+            }
+        }
     }
 }
