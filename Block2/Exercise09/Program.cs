59d7a15972816aa38294ea08e6ceca34d618f2a3
andregri <andrea.grillo96@live.com>
2016-10-19 14:03:55 +0200

    Add function to find legth of increasing sequences

--- Block2/Exercise09/Program.cs
+++ Block2/Exercise09/Program.cs
@@ -2,10 +2,34 @@
 
 namespace Exercise9
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+        }
+
+        public static int[] findLengthIncreasingSequence(int[] array)
+        {
+            int length = array.Length;
+            int[] lengthArray = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                lengthArray[i] = 1;
+            }
+
+            for (int x = 1; x < length; x++)
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    if (array[i] < array[x] && lengthArray[x] <= lengthArray[i])
+                    {
+                        lengthArray[x]++;
+                    }
+                }
+            }
+
+            return lengthArray;
         }
     }
 }
--- Block2/UnitTest/UnitTest09.cs
+++ Block2/UnitTest/UnitTest09.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise9;
 
 namespace UnitTest
 {
@@ -7,8 +8,16 @@ namespace UnitTest
     public class UnitTest09
     {
         [TestMethod]
-        public void TestMethod1()
+        public void findLengthIncreasingSequenceTest()
         {
+            int[] array = { 9, 6, 2, 7, 4, 7, 6, 5, 8, 4 };
+            int[] result = Program.findLengthIncreasingSequence(array);
+            int[] expected = { 1, 1, 1, 2, 2, 3, 3, 3, 4, 2 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i], "Index: "+ i);
+            }
         }
     }
 }
