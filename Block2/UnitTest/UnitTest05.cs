35b10e0a795a339c98f0ec1bc3baf107103bdee4
andregri <andrea.grillo96@live.com>
2016-10-21 09:00:09 +0200

    add test

--- Block2/UnitTest/UnitTest05.cs
+++ Block2/UnitTest/UnitTest05.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise5;
 
 namespace UnitTest
 {
@@ -7,8 +8,21 @@ namespace UnitTest
     public class UnitTest05
     {
         [TestMethod]
-        public void TestMethod1()
+        public void testN0andX1()
         {
+            double n = 0;
+            double x = 1;
+            double result = Program.getSum(n, x);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void testN2andX5()
+        {
+            double n = 2;
+            double x = 5;
+            double result = Program.getSum(n, x);
+            Assert.AreEqual(1.28d, result);
         }
     }
 }
