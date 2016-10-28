521a4e9af327936f271f3700f78b5f86586374fa
andregri <andrea.grillo96@live.com>
2016-10-28 22:21:12 +0200

    correct es03

--- Block2/Exercise03/Program.cs
+++ Block2/Exercise03/Program.cs
@@ -25,31 +25,50 @@ namespace Exercise3
                     Console.Write("Please enter the coefficient of known term: ");
                 } while (!Double.TryParse(Console.ReadLine(), out knownTerm));
 
-                solveQuadraticEq(firstCoefficient, secondCoefficient, knownTerm);
+                double[] roots = SolveQuadraticEq(firstCoefficient, secondCoefficient, knownTerm);
+
+                switch (roots.Length)
+                {
+                    case 2:
+                        Console.WriteLine("Roots are: {0:F2} and {1:F2}", roots[0], roots[1]);
+                        break;
+                    case 1:
+                        Console.WriteLine("The single root is: {0:F2}", roots[0]);
+                        break;
+                    default:
+                        Console.WriteLine("There aren't real roots");
+                        break;
+                }
             }
         }
 
-        public static void solveQuadraticEq(double a, double b, double c)
+        public static double[] SolveQuadraticEq(double a, double b, double c)
         {
-            double firstRoot, secondRoot;
+            double[] roots;
+            double discriminant = GetDiscriminant(a, b, c);
 
-            double discriminant = getDiscriminant(a, b, c);
-            if (discriminant >= 0)
+            if (discriminant > 0)
             {
-                firstRoot = (-b + Math.Sqrt(discriminant)) / (2 * a);
-                secondRoot = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                roots = new double[2];
 
-                Console.WriteLine("Real solution are: {0:f4} or {1:f4}\n", firstRoot, secondRoot);
+                roots[0] = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                roots[1] = (-b - Math.Sqrt(discriminant)) / (2 * a);
             }
+            else if (discriminant == 0)
+            {
+                roots = new double[1];
 
+                roots[0] = (-b + Math.Sqrt(discriminant)) / (2 * a);
+            }
             else
             {
-                Console.WriteLine("Equation hasn't real solution\n");
+                roots = new double[0];
             }
+
+            return roots;
         }
 
-        // creating for debugging purpose 
-        public static double getDiscriminant(double a, double b, double c)
+        public static double GetDiscriminant(double a, double b, double c)
         {
             return ((b * b) - (4 * a * c));
         }
--- Block2/UnitTest/UnitTest03.cs
+++ Block2/UnitTest/UnitTest03.cs
@@ -9,22 +9,45 @@ namespace UnitTest
         [TestMethod]
         public void TestPositiveDiscriminant()
         {
-            double delta = Program.getDiscriminant(4, 9, 2);
+            double delta = Program.GetDiscriminant(4, 9, 2);
             Assert.AreEqual(49, delta, "Error: result shouldn't be " + delta);
         }
 
         [TestMethod]
         public void TestZeroDiscriminant()
         {
-            double delta = Program.getDiscriminant(1, 2, 1);
+            double delta = Program.GetDiscriminant(1, 2, 1);
             Assert.AreEqual(0, delta, "Error: discriminant should be " + 0);
         }
 
         [TestMethod]
         public void TestNegativeDiscriminant()
         {
-            double delta = Program.getDiscriminant(1, 1, 1);
+            double delta = Program.GetDiscriminant(1, 1, 1);
             Assert.AreEqual(-3, delta, "Error: discriminant should be " + -3);
+        }
+
+        [TestMethod]
+        public void SolveQuadrEqTestDifferentRoots()
+        {
+            double[] expected = { 3d, 2d };
+            double[] roots = Program.SolveQuadraticEq(1, -5, 6);
+            CollectionAssert.AreEqual(expected, roots);
+        }
+
+        [TestMethod]
+        public void SolveQuadrEqTestSingleRoot()
+        {
+            double[] expected = { -1d };
+            double[] roots = Program.SolveQuadraticEq(1, 2, 1);
+            CollectionAssert.AreEqual(expected, roots);
+        }
+
+        [TestMethod]
+        public void SolveQuadrEqTestNotRealRoots()
+        {
+            double[] roots = Program.SolveQuadraticEq(1, 1, 1);
+            Assert.AreEqual(0, roots.Length);
         }
     }
 }
