9c37736baceff33587db198b2f80677f5b294759
andregri <andrea.grillo96@live.com>
2016-10-28 23:32:06 +0200

    correct es10 test

--- Block2/UnitTest/UnitTest10.cs
+++ Block2/UnitTest/UnitTest10.cs
@@ -7,150 +7,169 @@ namespace UnitTest
     [TestClass]
     public class UnitTest10
     {
+        public int[] GetRow(int[,] matrix, int row)
+        {
+            int rowLength = matrix.GetLength(1);
+            int[] expected = new int[rowLength];
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                expected[i] = matrix[row, i];
+            }
+
+            return expected;
+        }
+
         [TestMethod]
         public void InitMatrixATestSize2()
         {
-            int[,] result = { { 1, 3 }, { 2, 4 } };
-            int[,] actual = Program.InitMatrixA(2);
-            Assert.AreEqual(result[0, 0], actual[0, 0]);
-            Assert.AreEqual(result[1, 0], actual[1, 0]);
-            Assert.AreEqual(result[1, 1], actual[1, 1]);
-            Assert.AreEqual(result[0, 1], actual[0, 1]);
+            int[,] expected = { { 1, 3 }, { 2, 4 } };
+            int[,] matrix = Program.InitMatrixA(2);
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                CollectionAssert.AreEqual(GetRow(expected, i), GetRow(matrix, i));
+            }
         }
 
         [TestMethod]
         public void InitMatrixATestSize3()
         {
-            int[,] result = { { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } };
-            int[,] actual = Program.InitMatrixA(3);
-            Assert.AreEqual(result[0, 0], actual[0, 0]);
-            Assert.AreEqual(result[0, 1], actual[0, 1]);
-            Assert.AreEqual(result[0, 2], actual[0, 2]);
-            Assert.AreEqual(result[1, 0], actual[1, 0]);
-            Assert.AreEqual(result[1, 1], actual[1, 1]);
-            Assert.AreEqual(result[1, 2], actual[1, 2]);
-            Assert.AreEqual(result[2, 0], actual[2, 0]);
-            Assert.AreEqual(result[2, 1], actual[2, 1]);
-            Assert.AreEqual(result[2, 2], actual[2, 2]);
+            int[,] expected = { { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } };
+            int[,] matrix = Program.InitMatrixA(3);
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                CollectionAssert.AreEqual(GetRow(expected, i), GetRow(matrix, i));
+            }
         }
 
         [TestMethod]
         public void InitMatrixZigZagTestSize2()
         {
-            int[,] result = { { 1, 4 }, { 2, 3 } };
-            int[,] actual = Program.InitMatrixZigZag(2);
-            Assert.AreEqual(result[0, 0], actual[0, 0]);
-            Assert.AreEqual(result[1, 0], actual[1, 0]);
-            Assert.AreEqual(result[1, 1], actual[1, 1]);
-            Assert.AreEqual(result[0, 1], actual[0, 1]);
+            int[,] expected = { { 1, 4 }, { 2, 3 } };
+            int[,] matrix = Program.InitMatrixZigZag(2);
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                CollectionAssert.AreEqual(GetRow(expected, i), GetRow(matrix, i));
+            }
         }
 
         [TestMethod]
         public void InitMatrixZigZagTestSize3()
         {
-            int[,] result = { { 1, 6, 7 }, { 2, 5, 8 }, { 3, 4, 9 } };
-            int[,] actual = Program.InitMatrixZigZag(3);
-            Assert.AreEqual(result[0, 0], actual[0, 0]);
-            Assert.AreEqual(result[0, 1], actual[0, 1]);
-            Assert.AreEqual(result[0, 2], actual[0, 2]);
-            Assert.AreEqual(result[1, 0], actual[1, 0]);
-            Assert.AreEqual(result[1, 1], actual[1, 1]);
-            Assert.AreEqual(result[1, 2], actual[1, 2]);
-            Assert.AreEqual(result[2, 0], actual[2, 0]);
-            Assert.AreEqual(result[2, 1], actual[2, 1]);
-            Assert.AreEqual(result[2, 2], actual[2, 2]);
+            int[,] expected = { { 1, 6, 7 }, { 2, 5, 8 }, { 3, 4, 9 } };
+            int[,] matrix = Program.InitMatrixZigZag(3);
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                CollectionAssert.AreEqual(GetRow(expected, i), GetRow(matrix, i));
+            }
         }
 
         [TestMethod]
         public void InitMatrixDiagonalTestSize2()
         {
-            int[,] result = { { 2, 4 }, { 1, 3 } };
-            int[,] actual = Program.InitMatrixDiagonal(2);
-            Assert.AreEqual(result[0, 0], actual[0, 0]);
-            Assert.AreEqual(result[1, 0], actual[1, 0]);
-            Assert.AreEqual(result[1, 1], actual[1, 1]);
-            Assert.AreEqual(result[0, 1], actual[0, 1]);
+            int[,] expected = { { 2, 4 }, { 1, 3 } };
+            int[,] matrix = Program.InitMatrixDiagonal(2);
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                CollectionAssert.AreEqual(GetRow(expected, i), GetRow(matrix, i));
+            }
         }
 
         [TestMethod]
         public void InitMatrixDiagonalTestSize3()
         {
-            int[,] result = { { 4, 7, 9 }, { 2, 5, 8 }, { 1, 3, 6 } };
-            int[,] actual = Program.InitMatrixDiagonal(3);
-            Assert.AreEqual(result[0, 0], actual[0, 0]);
-            Assert.AreEqual(result[0, 1], actual[0, 1]);
-            Assert.AreEqual(result[0, 2], actual[0, 2]);
-            Assert.AreEqual(result[1, 0], actual[1, 0]);
-            Assert.AreEqual(result[1, 1], actual[1, 1]);
-            Assert.AreEqual(result[1, 2], actual[1, 2]);
-            Assert.AreEqual(result[2, 0], actual[2, 0]);
-            Assert.AreEqual(result[2, 1], actual[2, 1]);
-            Assert.AreEqual(result[2, 2], actual[2, 2]);
+            int[,] expected = { { 4, 7, 9 }, { 2, 5, 8 }, { 1, 3, 6 } };
+            int[,] matrix = Program.InitMatrixDiagonal(3);
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                CollectionAssert.AreEqual(GetRow(expected, i), GetRow(matrix, i));
+            }
         }
 
         [TestMethod]
         public void InitMatrixSpiralTestSize2()
         {
-            int[,] result = { { 0, 3 }, { 1, 2 } };
-            int[,] actual = Program.InitMatrixSpiral(2);
-            Assert.AreEqual(result[0, 0], actual[0, 0]);
-            Assert.AreEqual(result[1, 0], actual[1, 0]);
-            Assert.AreEqual(result[1, 1], actual[1, 1]);
-            Assert.AreEqual(result[0, 1], actual[0, 1]);
+            int[,] expected = { { 0, 3 }, { 1, 2 } };
+            int[,] matrix = Program.InitMatrixSpiral(2);
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                CollectionAssert.AreEqual(GetRow(expected, i), GetRow(matrix, i));
+            }
         }
 
         [TestMethod]
         public void InitMatrixSpiralTestSize3()
         {
-            int[,] result = { { 0, 7, 6 }, { 1, 8, 5 }, { 2, 3, 4 } };
-            int[,] actual = Program.InitMatrixSpiral(3);
-            Assert.AreEqual(result[0, 0], actual[0, 0]);
-            Assert.AreEqual(result[0, 1], actual[0, 1]);
-            Assert.AreEqual(result[0, 2], actual[0, 2]);
-            Assert.AreEqual(result[1, 0], actual[1, 0]);
-            Assert.AreEqual(result[1, 1], actual[1, 1]);
-            Assert.AreEqual(result[1, 2], actual[1, 2]);
-            Assert.AreEqual(result[2, 0], actual[2, 0]);
-            Assert.AreEqual(result[2, 1], actual[2, 1]);
-            Assert.AreEqual(result[2, 2], actual[2, 2]);
+            int[,] expected = { { 0, 7, 6 }, { 1, 8, 5 }, { 2, 3, 4 } };
+            int[,] matrix = Program.InitMatrixSpiral(3);
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                CollectionAssert.AreEqual(GetRow(expected, i), GetRow(matrix, i));
+            }
         }
 
         [TestMethod]
         public void GoDownTest()
         {
+            int[,] expected = { { 1, 0, 0 }, { 2, 0, 0 }, { 3, 0, 0 } };
+
             int[,] matrix = new int[3, 3];
             Program.GoDown(matrix, 0, 0, 3, 1);
-            Assert.AreEqual(1, matrix[0, 0]);
-            Assert.AreEqual(2, matrix[1, 0]);
-            Assert.AreEqual(3, matrix[2, 0]);
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                CollectionAssert.AreEqual(GetRow(expected, i), GetRow(matrix, i));
+            }
         }
 
         [TestMethod]
         public void GoUpTest()
         {
+            int[,] expected = { { 3, 0, 0 }, { 2, 0, 0 }, { 1, 0, 0 } };
+
             int[,] matrix = new int[3, 3];
             Program.GoUp(matrix, 0, 2, 3, 1);
-            Assert.AreEqual(3, matrix[0, 0]);
-            Assert.AreEqual(2, matrix[1, 0]);
-            Assert.AreEqual(1, matrix[2, 0]);
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                CollectionAssert.AreEqual(GetRow(expected, i), GetRow(matrix, i));
+            }
         }
 
         [TestMethod]
         public void GoRightTest()
         {
+            int[,] expected = { { 0, 1, 2 }, { 0, 0, 0 }, { 0, 0, 0 } };
+
             int[,] matrix = new int[3, 3];
             Program.GoRight(matrix, 0, 1, 2, 1);
-            Assert.AreEqual(1, matrix[0, 1]);
-            Assert.AreEqual(2, matrix[0, 2]);
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                CollectionAssert.AreEqual(GetRow(expected, i), GetRow(matrix, i));
+            }
         }
 
         [TestMethod]
         public void GoLeftTest()
         {
+            int[,] expected = { { 2, 1, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+
             int[,] matrix = new int[3, 3];
             Program.GoLeft(matrix, 0, 1, 2, 1);
-            Assert.AreEqual(1, matrix[0, 1]);
-            Assert.AreEqual(2, matrix[0, 0]);
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                CollectionAssert.AreEqual(GetRow(expected, i), GetRow(matrix, i));
+            }
         }
     }
 }
