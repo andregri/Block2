4e68172b7a316b81e620c493bd40a400db1f444b
andregri <andrea.grillo96@live.com>
2016-11-01 09:48:22 +0100

    change function test names

--- Block2/UnitTest/UnitTest02.cs
+++ Block2/UnitTest/UnitTest02.cs
@@ -9,7 +9,7 @@ namespace UnitTest
     public class UnitTest02
     {
         [TestMethod]
-        public void FibonacciTwentieth()
+        public void Fibonacci20()
         {
             BigInteger result = Program.Fibonacci(20)[19];
             Assert.AreEqual(4181, result, "Error: number shouldn't be " + result);
@@ -17,7 +17,7 @@ namespace UnitTest
 
 
         [TestMethod]
-        public void FibonacciFortieth()
+        public void Fibonacci40()
         {
             BigInteger result = Program.Fibonacci(40)[39];
             Assert.AreEqual(63245986, result, "Error: number shouldn't be " + result);
@@ -25,21 +25,21 @@ namespace UnitTest
 
 
         [TestMethod]
-        public void FibonacciSixtieth()
+        public void Fibonacci60()
         {
             BigInteger result = Program.Fibonacci(60)[59];
             Assert.AreEqual(956722026041, result, "Error: number shouldn't be " + result);
         }
 
         [TestMethod]
-        public void FibonacciEightieth()
+        public void Fibonacci80()
         {
             BigInteger result = Program.Fibonacci(80)[79];
             Assert.AreEqual(14472334024676221, result, "Error: number shouldn't be " + result);
         }
 
         [TestMethod]
-        public void FibonacciHundredth()
+        public void Fibonacci100()
         {
             BigInteger result = Program.Fibonacci(100)[99];
             BigInteger exp = BigInteger.Parse("218922995834555169026");
